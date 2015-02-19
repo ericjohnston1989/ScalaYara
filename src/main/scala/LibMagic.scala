4738d54c1e5e02ed74995576cb2a9978c7dceef7
Eric Johnston <eric.johnston@fireeye.com>
2015-02-19 14:38:35 -0800

    added file encoding + swapped Array[Byte] to ByteBuffer in JNA

--- src/main/scala/LibMagic.scala
+++ src/main/scala/LibMagic.scala
@@ -1,35 +1,37 @@
 package com.feye.ScalaYara
 
 import sna.Library
-import com.sun.jna.ptr.PointerByReference
-
 import com.sun.jna.Pointer
-
-//import com.sun.jna.Structure.ByReference
-
-import com.sun.jna.ptr.ByReference
+import java.nio.ByteBuffer
 
 class LibMagic(libraryLocation : String) { 
   
-  private val MAGIC_TYPE = 0
-  private val MAGIC_MIME = 16
+  private val MAGIC_TYPE = 0x000000
+  private val MAGIC_MIME = 0x000010
+  private val MAGIC_ENCODING = 0x000400
   
   val cLib = Library(libraryLocation)
   val magic_mime_cookie = cLib.magic_open(MAGIC_MIME)[Pointer]
   val magic_type_cookie = cLib.magic_open(MAGIC_TYPE)[Pointer]
+  val magic_encoding_cookie = cLib.magic_open(MAGIC_ENCODING)[Pointer]
   
   cLib.magic_load(magic_mime_cookie, null)[Int]
   cLib.magic_load(magic_type_cookie, null)[Int]
+  cLib.magic_load(magic_encoding_cookie, null)[Int]
   
-  def magicMime(binary : Array[Byte]) : String = { 
-    cLib.magic_buffer(magic_mime_cookie, binary, binary.length)[String]
+  def magicMime(binary : ByteBuffer) : String = {
+    cLib.magic_buffer(magic_mime_cookie, binary, binary.remaining)[String]
   }
   
-  def magicType(binary : Array[Byte]) : String = { 
-    cLib.magic_buffer(magic_type_cookie, binary, binary.length)[String]
+  def magicType(binary : ByteBuffer) : String = { 
+    cLib.magic_buffer(magic_type_cookie, binary, binary.remaining)[String]
   }
   
-  def close = { 
+  def magicEncoding(binary : ByteBuffer) : String = { 
+    cLib.magic_buffer(magic_encoding_cookie, binary, binary.remaining)[String]
+  }
+  
+  def close = {
     cLib.magic_close(magic_mime_cookie)[Unit]
     cLib.magic_close(magic_type_cookie)[Unit]
   }
--- src/test/scala/YaraTest.scala
+++ src/test/scala/YaraTest.scala
@@ -1,7 +1,7 @@
 package com.feye.ScalaYara
 
 import java.nio.file.{Files, Paths}
-
+import java.nio.ByteBuffer
 import org.specs2.mutable.Specification
 import sna.Library
 
@@ -40,15 +40,22 @@ class YaraTest extends Specification {
     "Generate the correct mime type" in { 
       val fake_file = Files.readAllBytes(Paths.get("/usr/bin/lsof"))
       val myLibMagic = new LibMagic("/usr/lib/x86_64-linux-gnu/libmagic.so.1")
-      val mime = myLibMagic.magicMime(fake_file)
+      val mime = myLibMagic.magicMime(ByteBuffer.wrap(fake_file))
       mime mustEqual "application/x-sharedlib"
     }
     
     "Generate the correct libmagic_type" in { 
       val fake_file = Files.readAllBytes(Paths.get("/usr/bin/lsof"))
       val myLibMagic = new LibMagic("/usr/lib/x86_64-linux-gnu/libmagic.so.1")
-      val mtype = myLibMagic.magicType(fake_file)
+      val mtype = myLibMagic.magicType(ByteBuffer.wrap(fake_file))
       mtype mustEqual "ELF 64-bit LSB shared object, x86-64, version 1 (SYSV)"
+    }
+    
+    "Generate the correct encoding" in { 
+      val fake_file = Files.readAllBytes(Paths.get("/usr/bin/lsof"))
+      val myLibMagic = new LibMagic("/usr/lib/x86_64-linux-gnu/libmagic.so.1")
+      val mencoding = myLibMagic.magicEncoding(ByteBuffer.wrap(fake_file))
+      mencoding mustEqual "binary"
     }
   }
   
